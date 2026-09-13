27e3200f38e030708b5e1fe91114ad08aac65439
velix-web <editvelix@gmail.com>
2026-09-13 10:36:54 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@ self.addEventListener('fetch', event => {
   if (url.origin !== location.origin) return;
   event.respondWith(
     fetch(event.request, {cache:'no-store'}).then(response => {
-      if (response.ok) caches.open(CACHE).then(c => c.put(event.request, response.clone()));
+      if (response.ok && response.status === 200) caches.open(CACHE).then(c => c.put(event.request, response.clone()));
       return response;
     }).catch(() => caches.match(event.request).then(cached => cached || (
       event.request.mode === 'navigate' ? caches.match('/') : new Response('', {status:504})
